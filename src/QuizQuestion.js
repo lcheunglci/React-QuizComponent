cb4b55d1a8a68f44986c49c1f8719ce06eefeaba
Lawrence LCI <lcheunglci@gmail.com>
2022-02-06 17:33:19 -0800

    Add event handling for the question buttons

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,6 +3,12 @@ import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
 
+    handleClick(button_text) {
+        if (button_text === this.props.quiz_question.answer) {
+            this.props.showNextQuestionHandler();
+        }
+    }
+
     render() {
         return <main>
         <section>
@@ -10,7 +16,8 @@ class QuizQuestion extends Component {
         </section>
         <section className="buttons">
           <ul>
-              <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
+              {this.props.quiz_question.answer_options.map((answer_option, index) => {
+                  return <QuizQuestionButton button_text={answer_option} key={index} clickHandler={this.handleClick.bind(this)} />})} 
             //quiz question button logic goes here
           </ul>
         </section>
